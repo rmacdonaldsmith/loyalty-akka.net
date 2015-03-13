8f24e33a3d8ed5222c235cd506943e61e0ea011e
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2015-03-13 10:21:56 -0700

    sketching out the process manager interactions

---
+++ src/LoyaltyService/PointsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace LoyaltyService
+{
+    public class PointsService : ReceiveActor
+    {
+        public PointsService()
+        {
+            Receive<Messages.Commands.CheckPointsBalance>(msg => { });
+        }
+    }
+}
--- src/LoyaltyService/RedemptionProcessManager.cs
+++ src/LoyaltyService/RedemptionProcessManager.cs
@@ -13,30 +13,41 @@ namespace LoyaltyService
 		GiftCardOrdered
 	}
 
-	public class Data
+	public class RedemptionProcessManager : ReceiveActor
 	{
-		public Guid RedemptionId { get; set; }
-		public States CurrentState { get; set; }
-		public long GPID { get; set; }
+	    private readonly Guid _redemptionId;
+	    private readonly ActorRef _redemptionStateActor;
+	    private readonly ActorRef _siftService;
+	    private readonly ActorRef _pointsService;
+	    private readonly ActorRef _giftService;
 
-		public Data (long gpid)
-		{
-			RedemptionId = Guid.NewGuid ();
-			GPID = gpid;
-		}
-	}
+        //pass in sift, points and gift services as they are totally stateless with respect to the redeption process; they dont care
+        //about the current redmptionIdS
+	    public RedemptionProcessManager (Guid redemptionId, ActorRef siftService, ActorRef pointsService, ActorRef giftService)
+	    {
+	        _redemptionId = redemptionId;
+	        _siftService = siftService;
+	        _pointsService = pointsService;
+	        _giftService = giftService;
+	        _redemptionStateActor = Context.ActorOf(Props.Create<RedemptionProcessState>(_redemptionId),"RedemptionStateActor");
 
-	public class RedemptionProcessManager : FSM<States, Data>
-	{
-		public RedemptionProcessManager ()
-		{
-			StartWith(States.StartingRedemption, new Data(0));
+		    Receive<Messages.Commands.StartOTGiftCardRedemption>(msg =>
+		        {
+                    _redemptionStateActor.Tell(msg, Self); //todo: is Self implicitly sent with Tell(), do I need to specify Self here?
+                    _siftService.Tell(new Messages.Commands.DoFraudCheck(), Self);
+		            return true;
+		        });
 
-//			When (States.PerformingFraudCheck, (Event<Data> fsmEvent) => {
-//			                                                                 return new State<States, Data>();
-//			});
+	        Receive<Messages.Events.OTGiftCardRedemptionStarted>(msg =>
+	            {
+	                
+	            });
 
+	        Receive<Messages.Events.FraudCheckPassed>(msg => _redemptionStateActor.Tell(msg));
 
+	        Receive<Messages.Events.FraudCheckFailed>(msg => _redemptionStateActor.Tell(msg));
+
+	        Receive<Messages.Events.FraudCheckPendingManualReview>(msg => _redemptionStateActor.Tell(msg));
 		}
 	}
 }
